02246273b415c5974aa9355f734993dd5a56ecc2
Julian Dario Gonzalez Toledo <juliant.2001@outlook.com>
2025-07-07 23:02:46 -0500

    fix: Se arregla interfaz e implementación para recibir IFormFilesCollection

--- Core/Interfaces/IFileService.cs
+++ Core/Interfaces/IFileService.cs
@@ -1,11 +1,12 @@
 using Core.Entities;
+using Microsoft.AspNetCore.Http;
 
 namespace Core.Interfaces;
 
 
 public interface IFileService
 {
-    Task RegisterFileAsync(FileEntity file);
+    Task RegisterFileAsync(IFormFileCollection files, string userId);
     Task<IEnumerable<FileEntity>> GetFilesAsync();
     Task<FileEntity?> GetFileAsync(Guid id);
 }
