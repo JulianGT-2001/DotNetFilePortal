b19a14caf200724a6f06dcbfac351edda3521f35
Julian Dario Gonzalez Toledo <juliant.2001@outlook.com>
2025-07-08 19:15:53 -0500

    docs: Se crea MyDbContext

---
+++ Infraestructure/MyDbContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Infraestructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace Core.Entities;
+
+public partial class MyDbContext : DbContext
+{
+    public MyDbContext()
+    {
+    }
+
+    public MyDbContext(DbContextOptions<MyDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<FileEntity> TbFiles { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseNpgsql("Server=localhost;Port=5432;Database=postgres;User Id=postgres;Password=Seguridad");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<FileEntity>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("tbf_id_pk");
+
+            entity.ToTable("tb_file");
+
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("id");
+            entity.Property(e => e.MimeType).HasColumnName("mime_type");
+            entity.Property(e => e.OriginalName).HasColumnName("original_name");
+            entity.Property(e => e.Path).HasColumnName("path");
+            entity.Property(e => e.SizeInBytes).HasColumnName("size_in_bytes");
+            entity.Property(e => e.UploadedAt)
+                .HasColumnType("timestamp without time zone")
+                .HasColumnName("uploaded_at");
+            entity.Property(e => e.UploadedBy).HasColumnName("uploaded_by");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
