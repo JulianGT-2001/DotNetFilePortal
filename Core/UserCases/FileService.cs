e11ae72a0619b9cafd498304e29d7c5c30cdbb79
Julian Dario Gonzalez Toledo <juliant.2001@outlook.com>
2025-07-08 20:15:23 -0500

    chore: Se implementa el método RegisterFileAsync y se almacena el archivo en el servidor.

--- Core/UserCases/FileService.cs
+++ Core/UserCases/FileService.cs
@@ -1,6 +1,7 @@
 using Core.Entities;
 using Core.Interfaces;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 
 namespace Core.UserCases;
 
@@ -8,17 +9,53 @@ public class FileService : IFileService
 {
     #region attributes
     private readonly IFileRepository _repository;
+    private readonly IConfiguration _configuration;
     #endregion
     #region constructor
-    public FileService(IFileRepository repository)
+    public FileService(IFileRepository repository, IConfiguration configuration)
     {
         _repository = repository;
+        _configuration = configuration;
     }
     #endregion
     #region methods
     public async Task RegisterFileAsync(IFormFileCollection files, string userId)
     {
-        throw new NotImplementedException();
+        List<FileEntity> filesEntity = new List<FileEntity>();
+        foreach (var file in files)
+        {
+            var fileName = Path.GetFileName(file.FileName);
+            var fileSize = file.Length;
+            var mimeType = file.ContentType;
+
+            var uniqueName = $"{Guid.NewGuid()}_{fileName}";
+            var principalRoute = _configuration["DownloadPath"]!;
+            var path = Path.Combine(principalRoute, uniqueName);
+
+            if (!Directory.Exists(principalRoute))
+            {
+                Directory.CreateDirectory(principalRoute);
+            }
+
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            var entity = new FileEntity
+            {
+                OriginalName = fileName,
+                Path = path,
+                SizeInBytes = fileSize,
+                MimeType = mimeType,
+                UploadedBy = userId,
+                UploadedAt = DateTime.UtcNow
+            };
+
+            filesEntity.Add(entity);
+        }
+
+        await _repository.AddFileAsync(filesEntity);
     }
 
     public Task<IEnumerable<FileEntity>> GetFilesAsync()
