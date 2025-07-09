c1dfd9182f8035ca3e833e494779cd34b041cc63
Julian Dario Gonzalez Toledo <juliant.2001@outlook.com>
2025-07-08 19:52:28 -0500

    fix: Se recibe una lista de FileEntity

--- Core/Interfaces/IFileRepository.cs
+++ Core/Interfaces/IFileRepository.cs
@@ -5,7 +5,7 @@ namespace Core.Interfaces;
 
 public interface IFileRepository
 {
-    Task AddFileAsync(FileEntity file);
+    Task AddFileAsync(List<FileEntity> file);
     Task<IEnumerable<FileEntity>> GetAllFilesAsync();
     Task<FileEntity?> GetFileByIdAsync(Guid id);
 }
