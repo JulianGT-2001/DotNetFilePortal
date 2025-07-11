509123cee9082e685f43ecdfad3d04de52cfb3dd
Julian Dario Gonzalez Toledo <juliant.2001@outlook.com>
2025-07-10 20:15:10 -0500

    chore: Se configura el jwt

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -10,10 +10,18 @@ DotNetEnv.Env.Load();
 
 var connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING");
 var downloadPath = Environment.GetEnvironmentVariable("DOWNLOAD_PATH");
+var jwtKey = Environment.GetEnvironmentVariable("JWT_KEY");
+var jwtIssuer = Environment.GetEnvironmentVariable("JWT_ISSUER");
+var jwtAudience = Environment.GetEnvironmentVariable("JWT_AUDIENCE");
+var jwtDuration = Environment.GetEnvironmentVariable("JWT_DURATION");
 
 
 builder.Configuration["ConnectionStrings:DefaultConnection"] = connectionString;
 builder.Configuration["DownloadPath"] = downloadPath;
+builder.Configuration["Jwt:Key"] = jwtKey;
+builder.Configuration["Jwt:Issuer"] = jwtIssuer;
+builder.Configuration["Jwt:Audience"] = jwtAudience;
+builder.Configuration["Jwt:Duration"] = jwtDuration;
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
