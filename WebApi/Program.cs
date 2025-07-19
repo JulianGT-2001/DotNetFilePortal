a333bae5bd3e2a2211cec9b3bd227567cde32865
Julian Dario Gonzalez Toledo <juliant.2001@outlook.com>
2025-07-18 22:22:16 -0500

    fix: Se elimina la restricción de deep en Program

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -33,12 +33,7 @@ builder.Configuration["Jwt:Duration"] = jwtDuration;
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddControllers()
-.AddJsonOptions(options =>
-    {
-        options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve;
-        options.JsonSerializerOptions.MaxDepth = 64; // Opcional: aumenta la profundidad máxima si es necesario
-    });
+builder.Services.AddControllers();
 builder.Services.AddDbContext<MyDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
