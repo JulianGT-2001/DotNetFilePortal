ecac3020b164b8bcdec8eef085c7e03ef44412d3
Julian Dario Gonzalez Toledo <juliant.2001@outlook.com>
2025-07-25 23:38:53 -0500

    feat: Se crea prueba para AddFileAsync comprobando si recibe un argumento nulo

--- fileupload.Tests/FileRepositoryTest.cs
+++ fileupload.Tests/FileRepositoryTest.cs
@@ -26,5 +26,16 @@ namespace fileupload.Tests
             mockDbContext.Verify(m => m.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Once);
             Assert.Empty(result);
         }
+
+        [Fact]
+        public async Task AddFileAsync_Throw_WhenArgumentIsNull()
+        {
+            var mockDbSet = new Mock<DbSet<FileEntity>>();
+            var mockDbContext = new Mock<MyDbContext>();
+
+            FileRepository fileRepository = new FileRepository(mockDbContext.Object);
+
+            await Assert.ThrowsAsync<ArgumentNullException>(() => fileRepository.AddFileAsync(null));
+        }
     }
 }
