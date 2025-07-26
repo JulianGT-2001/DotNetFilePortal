b021d6c707340ccbb068c7342031aeec75d70e4c
Julian Dario Gonzalez Toledo <juliant.2001@outlook.com>
2025-07-25 22:33:52 -0500

    feat: Se crean test unitarios nuevos para UserRepository

--- fileupload.Tests/UserRepositoryTest.cs
+++ fileupload.Tests/UserRepositoryTest.cs
@@ -26,8 +26,46 @@ namespace fileupload.Tests
             mockUserManager.Verify(m => m.CreateAsync(user, password), Times.Once);
         }
 
+        [Theory]
+        [InlineData(null, null)]
+        [InlineData("JulianGT", null)]
+        public async Task RegisterUserAsync_Throws_WhenDataIsInvalid(string userName, string Email)
+        {
+            var mockUserManager = new Mock<UserManager<ApplicationUser>>(
+                Mock.Of<IUserStore<ApplicationUser>>(), null, null, null, null, null, null, null, null);
+            mockUserManager.Setup(t => t.CreateAsync(It.IsAny<ApplicationUser>(), It.IsAny<string>()))
+            .ThrowsAsync(new ArgumentNullException("username"));
+            UserRepository userRepository = new UserRepository(mockUserManager.Object);
+            ApplicationUser user = new ApplicationUser()
+            {
+                UserName = userName,
+                Email = Email,
+                FullName = "Julian Gonzalez"
+            };
+
+            await Assert.ThrowsAsync<ArgumentNullException>(() => userRepository.RegisterUserAsync(user, "password"));
+        }
+
+        [Theory]
+        [InlineData(null, null)]
+        [InlineData("JulianGT", null)]
+        public async Task RegisterUserAsync_Throws_WhenDataIsValid(string userName, string Email)
+        {
+            var mockUserManager = new Mock<UserManager<ApplicationUser>>(
+                Mock.Of<IUserStore<ApplicationUser>>(), null, null, null, null, null, null, null, null);
+            UserRepository userRepository = new UserRepository(mockUserManager.Object);
+            ApplicationUser user = new ApplicationUser()
+            {
+                UserName = userName,
+                Email = Email,
+                FullName = "Julian Gonzalez"
+            };
+
+            await userRepository.RegisterUserAsync(user, "password");
+        }
+
         [Fact]
-        public void RegisterUserAsync_Throws_WhenUserManagerThrows()
+        public async Task RegisterUserAsync_Throws_WhenUserManagerThrows()
         {
             var mockUserManager = new Mock<UserManager<ApplicationUser>>(
                 Mock.Of<IUserStore<ApplicationUser>>(), null, null, null, null, null, null, null, null
@@ -36,7 +74,7 @@ namespace fileupload.Tests
             .ThrowsAsync(new Exception("Error"));
             UserRepository userRepository = new UserRepository(mockUserManager.Object);
 
-            Assert.ThrowsAsync<Exception>(() => userRepository.RegisterUserAsync(new ApplicationUser(), "password"));
+            await Assert.ThrowsAsync<Exception>(() => userRepository.RegisterUserAsync(new ApplicationUser(), "password"));
         }
     }
 }
